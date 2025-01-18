0a00cb36e3f9a97c0f1cb236bb20fcbcc30b4f86
Anusha Bhushaboina <bhushaboinaanushaias@gmail.com>
2025-01-18 16:21:23 +0530

    added flash messages for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,18 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     def new
+        @article = Article.new
     end
     def create
        @article = Article.new(params.require(:article).permit(:title, :description))
-       @article.save
+      if @article.save
     #    redirect_to article_path(@article)
-        redirect_to @article
+        redirect_to @article, notice: 'Article was successfully created.'
+      else
+        # render :new
+        # redirect_to new_article_path, status: :unprocessable_entity, alert: @article.errors.full_messages
+        flash[:alert] = @article.errors.full_messages
+        redirect_to new_article_path
+      end
     end
 end
